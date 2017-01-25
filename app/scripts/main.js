df915b5564d1f275ab854de9ff0c39d482152f7c
Alejandro Morente <morente5@hotmail.es>
2017-01-25 01:44:54 +0100

    Added Movie Grid styles and mouse events. Dummy Movie for testing purposes

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+$('.poster')
+	.on({
+		mouseenter: function() {
+			$(this).find('.poster-desc').stop().animate({
+				"height": "100%"
+			}, 200);
+		},
+		mouseleave: function() {
+			$(this).find('.poster-desc').stop().animate({
+				"height": 0
+			}, 200);
+		}
+	});
