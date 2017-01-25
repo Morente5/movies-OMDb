79aa003ca7179ec378999e6c0ce0d3dce782f854
Alejandro Morente <morente5@hotmail.es>
2017-01-25 01:49:47 +0100

    Update 2017-01-25T00:49:39.400Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";$(".poster").on({mouseenter:function(){$(this).find(".poster-desc").stop().animate({height:"100%"},200)},mouseleave:function(){$(this).find(".poster-desc").stop().animate({height:0},200)}});
